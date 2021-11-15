99b495f552d4fddeb2bcb2daef414dc983d4ed3e
Daniel Esteves <estevesd8@gmail.com>
2021-11-15 13:28:46 -0400

    feat: using dts instad of tsdx

--- src/index.tsx
+++ src/index.tsx
@@ -103,7 +103,7 @@ export function getYoutubeId(url: string): string {
   const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
   const match = url.match(regExp);
 
-  return match && match[7].length == 11 ? match[7] : '';
+  return match && match[7].length === 11 ? match[7] : '';
 }
 
 /**
