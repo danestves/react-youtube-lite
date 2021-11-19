677c217a4faf5d90ff9586d7ed6130979da3ddaf
Daniel Esteves <estevesd8@gmail.com>
2021-11-19 13:09:05 -0400

    chore(bump): update dependencies

--- test/ReactYouTubeLite.test.tsx
+++ test/ReactYouTubeLite.test.tsx
@@ -1,10 +1,14 @@
+// Dependencies
 import React from 'react';
 import * as ReactDOM from 'react-dom';
+
+// Internals
 import { Default as ReactYouTubeLite } from '../stories/ReactYouTubeLite.stories';
 
 describe('ReactYouTubeLite', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
+
     ReactDOM.render(
       <ReactYouTubeLite url="https://www.youtube.com/watch?v=DVQTGidS1yk" />,
       div
