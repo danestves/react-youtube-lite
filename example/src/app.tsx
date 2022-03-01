dfda744aa6e78ec71bbeac92176d88eed7161433
Daniel Esteves <me+github@danestves.com>
2022-03-01 03:46:30 -0400

    feat: correct youtube icon and example code

---
+++ example/src/app.tsx
@@ -0,0 +1,10 @@
+// Dependencies
+import { ReactYouTubeLite } from '../../src';
+
+export function App() {
+  return (
+    <div>
+      <ReactYouTubeLite url="https://www.youtube.com/watch?v=DVQTGidS1yk" />
+    </div>
+  );
+}
---
+++ example/src/index.tsx
@@ -0,0 +1,8 @@
+// Dependencies
+import ReactDOM from 'react-dom';
+
+// Internals
+import { App } from './App';
+
+const app = document.getElementById('app');
+ReactDOM.render(<App />, app);
